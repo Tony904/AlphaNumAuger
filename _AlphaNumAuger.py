827a454620b7d598d15813a28cd9e1093053dbd6
Tony904 <tnarde@gmail.com>
2022-04-06 17:17:15 -0400

    Add files via upload

--- _AlphaNumAuger.py
+++ _AlphaNumAuger.py
@@ -192,7 +192,6 @@ def blotcher1(src):
     img = cv2.dilate(img, (7, 7))
     img = cv2.erode(img, (3, 3))
 
-    # img = img[x:x+h+7, y-3:y+w+5+3].copy()
     img = cv2.resize(img, (0, 0), fx=10, fy=10, interpolation=cv2.INTER_NEAREST)
 
     img = gaussian_noise(img)
@@ -201,30 +200,28 @@ def blotcher1(src):
     img = cv2.GaussianBlur(img, (31, 31), cv2.BORDER_DEFAULT)
 
     img = cv2.resize(img, (0, 0), fx=1 / 10, fy=1 / 10, interpolation=cv2.INTER_AREA)
-    print("angle= " + str(angle))
     rads = angle * (math.pi / 180)
     ox = int(math.cos(rads) * 3)
-    print("ox= " + str(ox))
     oy = int(math.sin(rads) * 3)
-    print("oy= " + str(oy))
-    iters = 5
-    crop = np.full_like(img, 0)
-    crop = cv2.line(crop, (0, x + ox + 1), (img.shape[1], x + ox + 1), (0, 0, 50), 1)
-    crop = cv2.line(crop, (0, x + h + ox + 3), (img.shape[1], x + h + ox + 3), (0, 0, 50), 1)
-    crop = cv2.line(crop, (y - oy, 0), (y - oy, img.shape[0]), (0, 0, 50), 1)
-    crop = cv2.line(crop, (y + w - oy + 2, 0), (y + w - oy + 2, img.shape[0]), (0, 0, 50), 1)
+    # iters = 5
+    # crop = np.full_like(img, 0)
+    # crop = cv2.line(crop, (0, x + ox + 1), (img.shape[1], x + ox + 1), (0, 0, 50), 1)
+    # crop = cv2.line(crop, (0, x + h + ox + 3), (img.shape[1], x + h + ox + 3), (0, 0, 50), 1)
+    # crop = cv2.line(crop, (y - oy, 0), (y - oy, img.shape[0]), (0, 0, 50), 1)
+    # crop = cv2.line(crop, (y + w - oy + 2, 0), (y + w - oy + 2, img.shape[0]), (0, 0, 50), 1)
+    # crop2 = np.full_like(img, 0)
+    # crop2 = cv2.line(crop2, (0, x), (img.shape[1], x), (0, 50, 0), 1)
+    # crop2 = cv2.line(crop2, (0, x + h), (img.shape[1], x + h), (0, 50, 0), 1)
+    # crop2 = cv2.line(crop2, (y, 0), (y, img.shape[0]), (0, 50, 0), 1)
+    # crop2 = cv2.line(crop2, (y + w, 0), (y + w, img.shape[0]), (0, 50, 0), 1)
+    # img = img + crop + crop2
 
-    crop2 = np.full_like(img, 0)
-    crop2 = cv2.line(crop2, (0, x), (img.shape[1], x), (0, 50, 0), 1)
-    crop2 = cv2.line(crop2, (0, x + h), (img.shape[1], x + h), (0, 50, 0), 1)
-    crop2 = cv2.line(crop2, (y, 0), (y, img.shape[0]), (0, 50, 0), 1)
-    crop2 = cv2.line(crop2, (y + w, 0), (y + w, img.shape[0]), (0, 50, 0), 1)
-
-    img = img + crop + crop2
-
-    test = cv2.resize(img, (0, 0), fx=10, fy=10, interpolation=cv2.INTER_CUBIC)
-    debug_img(test, str(angle))
-    print(str(angle))
+    # img = img[x:x+h+7, y-3:y+w+5+3].copy()
+    top = x + ox + 1
+    bottom = x + h + ox + 6
+    left = y - oy
+    right = y + w - oy + 2
+    img = img[top:bottom, left:right].copy()
     return img
 
 
@@ -232,6 +229,76 @@ def sharpen(src, ksize=9):
     img = src.copy()
     img2 = cv2.GaussianBlur(img, (9, 9), cv2.BORDER_DEFAULT)
     img = cv2.addWeighted(img, 1.5, img2, -0.5, 0)
+    return img
+
+
+def trim(src):
+    img = src.copy()
+    row, col, ch = img.shape
+    trim_lines = np.full((row, col, ch), 0, dtype=np.uint8)
+    #  trim from top
+    thresh1 = 100
+    thresh2 = 255
+    for r in range(row - 1 - 3):
+        print("r=" + str(r))
+        row1 = img[r, 0:col].copy()
+        sigma3 = row1.mean() - 3 * row1.std()
+        print("row1 sigma3=" + str(sigma3))
+        print("row1.std=" + str(row1.std()))
+        print("row1.mean=" + str(row1.mean()))
+        if sigma3 > thresh1:  # checks if row 'r' contains pixels of the character (which are very dark)
+            row2 = img[r + 1, 0:col].copy()
+            row12 = cv2.addWeighted(row1, 0.5, row2, 0.5, 0)
+            row3 = img[r + 2, 0:col].copy()
+            row4 = img[r + 3, 0:col].copy()
+            row34 = cv2.addWeighted(row3, 0.5, row4, 0.5, 0)
+            if r > row // 2:
+                grad = row34 - row12
+            else:
+                grad = row12 - row34
+            gmean = grad.mean()
+            gstd = grad.std()
+            grad_1sig = gmean + gstd
+            print("grad_1sig=" + str(grad_1sig))
+            print("grad std=" + str(gstd))
+            print("grad mean=" + str(gmean))
+            if gmean < thresh2:  # if gradient is low then it must not be an edge
+                if r > row // 2:
+                    trim_lines = cv2.line(trim_lines, (0, r + 3), (col, r + 3), (0, 0, 255), 1)
+                else:
+                    trim_lines = cv2.line(trim_lines, (0, r), (col, r), (0, 0, 255), 1)
+
+                print("Drew line. r=" + str(r))
+    for c in range(col - 1 - 3):
+        print("c=" + str(c))
+        col1 = img[0:row, c].copy()
+        sigma3 = col1.mean() - 3 * col1.std()
+        print("col1 sigma3=" + str(sigma3))
+        print("col1.std=" + str(col1.std()))
+        print("col1.mean=" + str(col1.mean()))
+        if sigma3 > thresh1:  # checks if col 'c' contains pixels of the character (which are very dark)
+            col2 = img[0:row, c + 1].copy()
+            col12 = cv2.addWeighted(col1, 0.5, col2, 0.5, 0)
+            col3 = img[0:row, c + 2].copy()
+            col4 = img[0:row, c + 3].copy()
+            col34 = cv2.addWeighted(col3, 0.5, col4, 0.5, 0)
+            if c > col // 2:
+                grad = col34 - col12
+            else:
+                grad = col12 - col34
+            gmean = grad.mean()
+            gstd = grad.std()
+            grad_1sig = gmean + gstd
+            print("grad_1sig=" + str(grad_1sig))
+            print("grad std=" + str(gstd))
+            print("grad mean=" + str(gmean))
+            if gmean < thresh2:  # if gradient is low then it must not be an edge
+                if c > col // 2:
+                    trim_lines = cv2.line(trim_lines, (c + 3, 0), (c + 3, row), (0, 0, 255), 1)
+                else:
+                    trim_lines = cv2.line(trim_lines, (c, 0), (c, row), (0, 0, 255), 1)
+                print("Drew line. c=" + str(c))
+    img = cv2.addWeighted(img, 1, trim_lines, 1, 0)
     return img
 
 
@@ -287,7 +354,9 @@ def process_images_2():
             img = cv2.dilate(img, (3, 3), iterations=3)
             img = cv2.erode(img, (3, 3), iterations=3)
             img = gaussian_blur(img)
+            img = trim(img)
             cv2.imwrite(save_dir + '_auged_' + filename + '.png', img)
+            print("Saved " + save_dir + '_auged_' + filename + '.png')
 
 
 def testing():
